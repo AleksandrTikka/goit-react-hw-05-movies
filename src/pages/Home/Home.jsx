f1d3ed3f7c0512d5cf9a90f99722c93c10eefa86
AleksandrTikka <alexandertikka@gmail.com>
2022-09-26 00:45:16 +0300

    home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,28 @@
+import { useState, useEffect } from 'react';
+import { fetchTrending } from 'services/api';
+
 const Home = () => {
-  return <></>;
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function getMovies() {
+      const response = await fetchTrending();
+      console.log(response);
+      setMovies(response);
+    }
+    getMovies();
+  }, []);
+
+  return (
+    <div>
+      {movies.length > 0 && (
+        <ul>
+          {movies.map(movie => {
+            return <li key={movie.id}>{movie.title}</li>;
+          })}
+        </ul>
+      )}
+    </div>
+  );
 };
 export default Home;
--- src/services/api.js
+++ src/services/api.js
@@ -4,12 +4,9 @@ Axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
 const API_KEY = 'a56dfce00590e00de6e7abfd96a9065d';
 
 export const fetchTrending = async () => {
-  const params = new URLSearchParams({
-    api_key: API_KEY,
-  });
   try {
-    const response = await Axios.get('trending/movie/day', params);
-    return response.data;
+    const response = await Axios.get(`trending/movie/day?api_key=${API_KEY}`);
+    return response.data.results;
   } catch (error) {
     console.log(error);
   }
@@ -25,7 +22,7 @@ export const fetchSearchMovies = async query => {
   });
   try {
     const response = await Axios.get('search/movie', params);
-    return response.data;
+    return response.data.results;
   } catch (error) {
     console.log(error);
   }
@@ -35,6 +32,24 @@ export const fetchMovieDetails = async movieId => {
   try {
     const response = await Axios.get(`movie/${movieId}?${API_KEY}`);
     return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const fetchMovieCast = async movieId => {
+  try {
+    const response = await Axios.get(`movie/${movieId}/credits?${API_KEY}`);
+    return response.data.cast;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const fetchMovieReview = async movieId => {
+  try {
+    const response = Axios.get(`movie/${movieId}/review?${API_KEY}`);
+    return response.data.results;
   } catch (error) {
     console.log(error);
   }
