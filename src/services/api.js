1b08eb55e1ef9eef065c3cb44211038250e9831a
AleksandrTikka <alexandertikka@gmail.com>
2022-09-25 13:09:33 +0300

    api began

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,13 @@
 import Axios from 'axios';
 axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
-axios.defaults.headers.common['a56dfce00590e00de6e7abfd96a9065d'] = AUTH_TOKEN;
+const API_KEY = 'a56dfce00590e00de6e7abfd96a9065d';
 
 export const fetchTrending = async () => {
+  const params = new URLSearchParams({
+    api_key: API_KEY,
+  });
   try {
-    const response = await Axios.get('trending/movie/day');
+    const response = await Axios.get('trending/movie/day', params);
     return response.data;
   } catch (error) {
     console.log(error);
@@ -14,12 +17,12 @@ export const fetchTrending = async () => {
 // https://api.themoviedb.org/3/trending/movie/day?api_key=<<api_key>>
 
 export const fetchSearchMovies = async query => {
+  const params = new URLSearchParams({
+    language: 'en-US',
+    query,
+    page: 1,
+  });
   try {
-    const params = new URLSearchParams({
-      language: 'en-US',
-      query: 'query',
-      page: 1,
-    });
     const response = await Axios.get('search/movie', params);
     return response.data;
   } catch (error) {
